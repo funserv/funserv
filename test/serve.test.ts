f14c0c752bc94ad8b6d027916595e2074761d54b
Maxime Bernard <maxime@kymono.co>
2024-06-17 16:09:34 +0200

    chore: update all dependencies

--- test/serve.test.ts
+++ test/serve.test.ts
@@ -1,12 +1,16 @@
-/* eslint-disable jest/no-test-callback */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/promise-function-async */
 import { serve } from '../src/serve';
 import request from 'supertest';
 
 describe('serve()', () => {
-  it('serve(undefined) should return 200', (done) =>
-    request(serve(undefined)).get('/').expect(200, done));
+  it('serve(undefined) should return 200', () =>
+    new Promise((done) =>
+      request(serve(undefined)).get('/').expect(200, done),
+    ));
 
-  it('serve(Hello World) should return 200', (done) =>
-    request(serve('Hello World')).get('/').expect(200, 'Hello World', done));
+  it('serve(Hello World) should return 200', () =>
+    new Promise((done) =>
+      request(serve('Hello World')).get('/').expect(200, 'Hello World', done),
+    ));
 });
